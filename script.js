243fa6cbc9349306eb918feef7bab755f0ae6dca
GoMenXGames <46220650+GoMenXGames@users.noreply.github.com>
2023-01-01 07:05:44 +0500

    green color, row style
    add green glow, and row style

--- script.js
+++ script.js
@@ -2,29 +2,36 @@ function check() {
     retry = document.querySelector("input#iretry").value;
     factors = document.querySelector("input#ifactors").value;
 
-    console = document.querySelector("#console");
+    output = document.querySelector("#output");
 
     button = document.querySelector("button");
 
     alert("Check!");
+    output.innerHTML = "<div id=\"console\">test</div>";
+    console = document.querySelector("#console");
     console.innerHTML = "";
     gscore = 0;
     for (r = 1; r <= retry; r++) {
-        print("Попытка " + r + "<br/>");
         groupFactors = [];
+        html = "<div id='"+r+"'> Попытка " + r + "<br/>";
         for (f = 1; f <= factors; f++) {
             groupFactors.push(Math.round(Math.random()));
-            print("F"+ f+ ": " + groupFactors[f-1] + "<br/>");
+            html += ("F"+ f+ ": " + groupFactors[f-1] + "<br/>");
         }
+        html += "</div>";
+        print(html);
         score = 0;
         groupFactors.forEach(fact => {
             if (fact == 1) score++;
         })
-        if (score == groupFactors.length) gscore++;
+        if (score == groupFactors.length) {
+            gscore++;
+            console.querySelectorAll("div")[r-1].classList.add("green");
+        }
     }
-    print("Итог " + gscore + " из " + retry);
-    percents = Math.floor(gscore  / retry * 10000) / 100 
-    print(" ("+percents+"%)")
+    output.innerHTML += ("Итог " + gscore + " из " + retry);
+    percents = Math.floor(gscore  / retry * 10000) / 100 ;
+    output.innerHTML += (" ("+percents+"%)");
 }
 
 function print(text) {
